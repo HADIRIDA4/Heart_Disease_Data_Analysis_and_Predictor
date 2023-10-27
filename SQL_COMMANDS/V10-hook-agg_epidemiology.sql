3509b387ecfddfe696536bf8df17194d3565711e
HADIRIDA4 <hadirida444@gmail.com>
2023-10-27 06:53:40 +0300

    setting update or insert

--- SQL_COMMANDS/V10-hook-agg_epidemiology.sql
+++ SQL_COMMANDS/V10-hook-agg_epidemiology.sql
@@ -1,29 +1,33 @@
+
 CREATE TABLE IF NOT EXISTS target_schema.fct_epidemiology
 (
   measure text,
-    location_name text,
-    sex text,
-    age_interval text,
-    cause text,
-    metric text,
-    year INT,
-    val float,
-    last_update timestamp
+  location_name text,
+  sex text,
+  age_interval text,
+  cause text,
+  metric text,
+  year INT,
+  val float,
+  last_update timestamp
 );
-
 
 
 INSERT INTO target_schema.fct_epidemiology
 (
   SELECT
-    measure,
-    location ,
-    sex ,
-    age ,
-    cause,
-    metric,
-    year,
-    val,
-    last_update
-  FROM target_schema.stg_heart_db_epidemiology
+    stg.measure,
+    stg.location,
+    stg.sex,
+    stg.age,
+    stg.cause,
+    stg.metric,
+    stg.year,
+    stg.val,
+    stg.last_update
+  FROM target_schema.stg_heart_db_epidemiology stg
 )
+
+ON CONFLICT (last_update)
+DO UPDATE SET
+  val = EXCLUDED.val;
