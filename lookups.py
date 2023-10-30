f7f91d9ca270c5b43c56834224ac4096b163518d
HADIRIDA4 <hadirida444@gmail.com>
2023-10-30 04:23:18 +0200

    deleted unwanted lookups items

--- lookups.py
+++ lookups.py
@@ -23,7 +23,7 @@ class ErrorHandling(Enum):
     HOOK_SQL_ERROR = "Hook: SQL Error"
     DATE_CONVERSION_ERROR = "Warning: column is not a date format"
     RETURN_ETL_LAST_UPDATE_ERROR = "Error returning ETL last update"
-    
+
 
 class InputTypes(Enum):
     SQL = "SQL"
@@ -32,35 +32,33 @@ class InputTypes(Enum):
     HTTP = "Http"
 
 
-   
-
-class studies(Enum):
-    FRAMINGHAM_STUDY = "csv/Framingham_Study.csv"
-    SWITZERLAND = "csv/Switzerland.csv"
-    CLEVELAND = "csv/CVD_cleaned.csv"
-    DISEASE = "csv/Disease.csv"
-    CONFUSION_PREDICTION = "csv\confusion_results.csv"
-    METRICS_RANKING = "csv\metrics_results.csv"
-    FEATURE_IMPORTANCE = "csv\important_features_results.csv"     
-    MODEL_RESULT_CSV_PATH = "csv\models_result.csv"
-    EPIDEMIOLOGY="csv\epidemiological_data.csv"
-
-
-
 class links(Enum):
-  DISEASE_CATEGORY ="https://drive.google.com/file/d/11DNcehV12u6YCt_lMpuL5FAP3PuRx3Ri/view?usp=drive_link"
-  DISEASE ="https://drive.google.com/file/d/13ms6gD8NdZNo0X8n9NV-ncTX-MDZHAci/view?usp=sharing"
-  EPIDEMIOLOGY="https://drive.google.com/file/d/1DxJzOuell6qIxZaSwIrvkjQpU-AXktbu/view?usp=sharing"
-  FRAMINGHAM_STUDY="https://drive.google.com/file/d/1Ab5vh45YOEHclEqnMogO9BcYA5_qTkEg/view?usp=sharing"
-  CLEVELAND_PREDICTION ="https://drive.google.com/file/d/1ZngQXrwS7xxxq-Yjp_tJtgY-2c5l9tr3/view?usp=drive_link"
-  PREDICTION="https://drive.google.com/file/d/1Uo0Kt_oKFL6aI-a_uqV8HfMu1HQ6aXQo/view?usp=sharing"
-  LOCATION="https://drive.google.com/file/d/1rIfQRJRXaugRoj1Xw4nYZZS7IYG3-3Jk/view?usp=drive_link"
-  MEASURES="https://drive.google.com/file/d/1_fT69Pprh_HwDUWjj0tO4QUCqfn02gjh/view?usp=sharing"
-  SWITZERLAND_STUDY="https://drive.google.com/file/d/1xD0DYCNhvqpcYHX3fpoeCKFLqHLenk1f/view?usp=drive_link"
-  CVD_CLEANED="https://drive.google.com/file/d/1sSAfmx4jmjBWwgkJ9H7dTylrZu5zKofl/view?usp=drive_link"
-  
+    DISEASE_CATEGORY = (
+        "https://drive.google.com/uc?id=1wKg3cAFfZPPF36Li-MQX-EEA7ZLXT_wv"
+    )
+    DISEASE = "https://drive.google.com/uc?id=1CCTSodS_A6XcCXKTkbcRy__H33X5NAID"
+    EPIDEMIOLOGY = "https://drive.google.com/uc?id=1ujGL5F3V7PMaYSo7BtJ0AGHX2kgJg_3m"
+    FRAMINGHAM_STUDY = (
+        "https://drive.google.com/uc?id=1--HEe4xJgPP2EAE-2V8WC3l0WBqBVI_f"
+    )
+    LOCATION = "https://drive.google.com/uc?id=13KvsA8rPVHiYtyyI9wTTGymFjnMk5u3j"
+    MEASURES = "https://drive.google.com/uc?id=1BpEenYp0pZw0DAEajMJV39a-_A_IpRew"
+    SWITZERLAND_STUDY = (
+        "https://drive.google.com/uc?id=1XF9GZjP5xw1jjUCx3FhVT3AHPs2DbiY4"
+    )
+    CVD_CLEANED = "https://drive.google.com/uc?id=11yBqPXzNcOIfcgKtlCgMX8i4fno3OIq9"
+    confusion_results = (
+        "https://drive.google.com/uc?id=1itIYB75d0Su-46Ng59aqcGFiYxiJFZkU"
+    )
+    importance_features = (
+        "https://drive.google.com/uc?id=1DKCnrDq1IwHO6Ln10Ip6SY9nelz56rf0"
+    )
+    metrics_reults = "https://drive.google.com/uc?id=15gxFEZZBPmj-_5giz1Ncv-k8ra7sdJ9s"
+    models_result = "https://drive.google.com/uc?id=12n4-FkXppt34uOgLVvgXiKVPm8O0nnAk"
 
 
+class prediction_link(Enum):
+    PREDICTION = "https://drive.google.com/uc?id=1ZngQXrwS7xxxq-Yjp_tJtgY-2c5l9tr3"
 
 
 class DestinationDatabase(Enum):
@@ -68,19 +66,19 @@ class DestinationDatabase(Enum):
     DATABASE_NAME = "heart_db"
 
 
-
-
+class ETL_CHECK_POINT_TABLE(Enum):
+    ETL_CHECKPOINT = "etl_checkpoint"
 
 
 class ETLStep(Enum):
     PRE_HOOK = "prehook"
     HOOK = "hook"
-    POSTHOOK="posthook"
+    POSTHOOK = "posthook"
 
 
 class ML_TYPE:
-    NON_LINEAR="NON_LINEAR_ML"
-    LINEAR_ML="LINEAR_ML"
+    NON_LINEAR = "NON_LINEAR_ML"
+    LINEAR_ML = "LINEAR_ML"
 
 
 class ChestPainType(Enum):
@@ -89,21 +87,23 @@ class ChestPainType(Enum):
     NON_ANGINAL_PAIN = 3
     ASYMPTOMATIC = 4
 
+
 class RestECGType(Enum):
     NORMAL = 0
     ST_T_WAVE_ABNORMALITY = 1
     LEFT_VENTRICULAR_HYPERTROPHY = 2
+
 
 class StSlopeType(Enum):
     UPSLOPING = 1
     FLAT = 2
     DOWNSLOPING = 3
 
+
 class SexType(Enum):
     FEMALE = 0
     MALE = 1
 
 
-
 class Output_Directory(Enum):
-    CSV="csv"
+    CSV = "csv"
