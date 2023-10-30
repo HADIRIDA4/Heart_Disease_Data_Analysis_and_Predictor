a81273e30250d077f1bfbd67e1e3889138502a56
HADIRIDA4 <hadirida444@gmail.com>
2023-10-30 15:57:45 +0200

    added data ingestion file

---
+++ data_ingestion_script.py
@@ -0,0 +1,18 @@
+import logging
+
+logging.basicConfig(
+    filename="execution.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
+
+
+from prehook import execute_prehook
+from hook import *
+from posthook import *
+
+
+execute_prehook()
+
+execute_hook()
+execute_posthook()
